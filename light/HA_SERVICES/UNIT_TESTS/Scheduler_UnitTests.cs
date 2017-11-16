f590bd04b50d3289e96f8e6e7ac19275041d718a
Georg Derflinger <georgderflinger@gmail.com>
2017-11-16 07:54:47 +0100

    removed unused usings

--- light/HA_SERVICES/UNIT_TESTS/Scheduler_UnitTests.cs
+++ light/HA_SERVICES/UNIT_TESTS/Scheduler_UnitTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using Moq;
+﻿using NUnit.Framework;
 using Scheduler;
 
 namespace Communication.HA_SERVICES.UNIT_TESTS
