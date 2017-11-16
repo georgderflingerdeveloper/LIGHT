ce5295efe2a971027858a82af343ed4f343780ab
Georg Derflinger <georgderflinger@gmail.com>
2017-11-16 07:53:43 +0100

    ignoring of this unit test file was not intended

---
+++ light/HA_SERVICES/UNIT_TESTS/Scheduler_UnitTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using Scheduler;
+
+namespace Communication.HA_SERVICES.UNIT_TESTS
+{
+    [TestFixture]
+    public class Scheduler_UnitTests
+    {
+        [SetUp]
+        public void Init()
+        {
+        }
+
+        string TestDays                               = "Mon,Tue,Wed,Thu,Fri,Sat,Sun";
+        string TestTimePointZeroSeconds               = "04:30:00";
+        string ExpectedResultOfCroneStringZeroSeconds = "0 30 04 ? * Mon,Tue,Wed,Thu,Fri,Sat,Sun";
+
+        string TestTimePoint = "04:30:33";
+        string ExpectedResultOfCroneString = "33 30 04 ? * Mon,Tue,Wed,Thu,Fri,Sat,Sun";
+
+        string TestTimePointWrongSecond = "04:30:60";
+
+
+        [Test]
+        public void TestCroneTimePoint_Any()
+        {
+            string CroneResult = MyCroneConverter.GetPointOfTime( TestTimePoint, TestDays );
+            Assert.AreEqual( ExpectedResultOfCroneString, CroneResult );
+        }
+
+        [Test]
+        public void TestCroneTimePoint_ZeroSeconds()
+        {
+            string CroneResult = MyCroneConverter.GetPointOfTime( TestTimePointZeroSeconds, TestDays );
+            Assert.AreEqual( ExpectedResultOfCroneStringZeroSeconds, CroneResult );
+        }
+
+        [Test]
+        public void TestCroneTimePoint_WithWrongSecondTimeFormat()
+        {
+            string CroneResult = MyCroneConverter.GetPointOfTime( TestTimePointWrongSecond, TestDays );
+            Assert.AreEqual( ExpectedResultOfCroneStringZeroSeconds, CroneResult );
+        }
+
+
+        [TearDown]
+        public void Cleanup()
+        {
+        }
+
+    }
+}
