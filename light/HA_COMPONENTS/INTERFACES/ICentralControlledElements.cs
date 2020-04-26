9ff26db4a0e6fcc4d980ed03ebc7ffded0ed2612
Georg Derflinger <georgderflinger@gmail.com>
2020-04-26 16:48:19 +0200

    new interface

---
+++ light/HA_COMPONENTS/INTERFACES/ICentralControlledElements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Communication.HA_COMPONENTS.INTERFACES
+{
+    public delegate void UpdateOutputs(object sender, bool[] _DigOut, List<int> match);
+
+    interface ICentralControlledElements
+    {
+        void DelayedDeviceOnRisingEdge(bool Value);
+        void DelayedDeviceOnFallingEdge(bool Value);
+        void DeviceOnFallingEdgeAutomaticOff(bool Value);
+
+        event UpdateOutputs EUpdateOutputs;
+
+    }
+}
